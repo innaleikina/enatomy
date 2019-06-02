ea91653441e79090845c89853277f93a90e56e3c
Inna <designbyinna@gmail.com>
2019-06-02 11:23:47 -0600

    home images are randomized

--- client/src/pages/Main/Main.js
+++ client/src/pages/Main/Main.js
@@ -34,10 +34,33 @@ class Main extends Component {
     this.getFiles()
  }
 
+
+ shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+
+
    renderItems = () => {
     //  console.log(this.state.fileNameArr);
      let jpgCoverShotsArr = []
-     
+     this.shuffle(this.state.fileNameArr);
+    
     for(var i=0; i < this.state.fileNameArr.length; i++){
       // console.log(this.state.fileNameArr[i]);
       if(this.state.fileNameArr[i].filename.endsWith("jpg")){
