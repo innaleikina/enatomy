00a3e9f85f70deb3207af957f493d3ccb6d81459
Inna <designbyinna@gmail.com>
2019-06-02 11:11:57 -0600

    home images come from the s3 database

--- client/src/pages/Main/Main.js
+++ client/src/pages/Main/Main.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import AboutUs from "../../components/AboutUs";
 import WhatIs from "../../components/WhatIs";
 import HomeImage from "../../components/HomeImage";
+import { Link } from 'react-router-dom';
+import API from "../../utils/API";
+
 
 
 import './main.css';
@@ -12,18 +15,47 @@ import './main.css';
 class Main extends Component {
 
   state = {
-    photosArr:[]
+    photosArr:[],
+    fileNameArr:"",
 
+   }
+
+   getFiles = () => {
+    API.getFiles() 
+      .then(res => this.setState({
+        fileNameArr:res.data
+      }))
+      .catch(err => console.log(err))
+  };
+
+
+
+  componentWillMount() {
+    this.getFiles()
+ }
+
+   renderItems = () => {
+    //  console.log(this.state.fileNameArr);
+     let jpgCoverShotsArr = []
+     
+    for(var i=0; i < this.state.fileNameArr.length; i++){
+      // console.log(this.state.fileNameArr[i]);
+      if(this.state.fileNameArr[i].filename.endsWith("jpg")){
+        //console.log("this is a jpg file " + this.state.fileNameArr[i].filename);
+        jpgCoverShotsArr.push(<Link key={this.state.fileNameArr[i].filename} to={`/set/${this.state.fileNameArr[i].filename}`}><HomeImage key={this.state.fileNameArr[i].filename} fileName={this.state.fileNameArr[i].filename}>
+        </HomeImage></Link>)
+      } else {
+       // console.log("this is not a jpg file " + this.state.fileNameArr[i].filename)
+      }
+    }
+    return jpgCoverShotsArr
    }
 
     render() {
         return (
          <div > 
             <div className="home-image-container">
-              <HomeImage link="https://enatomy-home.s3.amazonaws.com/home-1.png"></HomeImage>
-              <HomeImage link="https://enatomy-home.s3.amazonaws.com/home-2.jpg"></HomeImage>
-              <HomeImage link="https://enatomy-home.s3.amazonaws.com/home-3.png"></HomeImage>
-              <HomeImage link="https://enatomy-home.s3.amazonaws.com/home-4.png"></HomeImage>
+            {this.renderItems()}
             </div>
             <div className="about-container">
              <AboutUs></AboutUs>
