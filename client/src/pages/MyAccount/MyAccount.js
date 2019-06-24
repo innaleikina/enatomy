388fac9c9c1958c5fbdbdd0737648db33b9e4607
Inna <designbyinna@gmail.com>
2019-06-23 20:34:06 -0600

    my account styling

--- client/src/pages/MyAccount/MyAccount.js
+++ client/src/pages/MyAccount/MyAccount.js
@@ -56,33 +56,35 @@ class MyAccount extends Component {
   render() {
     return (
         <div id="timeline-wrap" >
-           <span className="title-account"> {this.state.user.name}'s account </span>
-           
-          
 
-           <h3>Settings </h3>
-           {/* when button is clicked a pop up shows up to change password */}
-           <button> change password </button>
+          <div className="top-account">
+              <span className="title-account"> {this.state.user.name}'s account </span>
+              
+              
 
-            { /* pop up to confirm */}
-           <button> delete account </button>
+              <h3>Settings </h3>
+              {/* when button is clicked a pop up shows up to change password */}
+              <button> change password </button>
 
+                { /* pop up to confirm */}
+                <button> delete account </button>
+           </div>
           
            {/* render sets that have been purchased before, if lcicked go to imgae page */}
            <div className="purchased-sets">
-           <h4 className="title-sets"> Purchased Sets </h4>
-           <div className="purchased-all-imgs">
-           {(this.state.imagesPurchased.length > 0) ? 
-                  this.state.imagesPurchased.map((image,index) => (
-                    <div className="purchased-img" key={index}>
-                      <Link key={index} to={`/set/${image}`}>
-                          <CoverPhoto key={index} fileName={image}>
-                          </CoverPhoto>
-                      </Link> 
-                    </div>
-                  ))
-                  : <div>You haven't purchased any sets yet</div>}
-           </div>
+              <h4 className="title-sets"> Purchased Sets </h4>
+              <div className="purchased-all-imgs">
+                  {(this.state.imagesPurchased.length > 0) ? 
+                          this.state.imagesPurchased.map((image,index) => (
+                            <div className="purchased-img" key={index}>
+                              <Link key={index} to={`/set/${image}`}>
+                                  <CoverPhoto key={index} fileName={image}>
+                                  </CoverPhoto>
+                              </Link> 
+                            </div>
+                          ))
+                          : <div>You haven't purchased any sets yet</div>}
+              </div>
            </div>
         </div>
 
